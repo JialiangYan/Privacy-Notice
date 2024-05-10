8074d25fd28f0dcae0a305d8af79a882c2e2621a
yajialiang <240881275@qq.com>
2024-05-09 23:32:50 -0600

    store page

--- src/components/Slide/index.js
+++ src/components/Slide/index.js
@@ -1,7 +1,23 @@
+import React from 'react'
 import styles from './index.module.css'
 
-function Slide() {
-  return <div className="block">Slide</div>
+function Slide({ images }) {
+  return (
+    <div class={styles.container}>
+      <div class={styles.scroll}>
+        <div class={styles.row}>
+          {images.map((image, index) => (
+            <img
+              key={index}
+              className={styles.img}
+              src={image}
+              alt={`Screenshot ${index + 1}`}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Slide
--- src/pages/Store/index.js
+++ src/pages/Store/index.js
@@ -1,4 +1,4 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import gsap from 'gsap'
 import { useGSAP } from '@gsap/react'
 
@@ -6,43 +6,65 @@ import styles from './index.module.css'
 
 import Slide from '../../components/Slide'
 
-import up from '../../assets/up.png'
+import up from '../../assets/store/up.png'
+import s1 from '../../assets/store/ss1.png'
+import s2 from '../../assets/store/ss2.png'
+import s3 from '../../assets/store/ss3.png'
+import device from '../../assets/store/device.png'
+import bottom from '../../assets/store/bottom.png'
 
 export default function Store() {
   const tl = gsap.timeline()
   const storeToapp = useRef()
 
-  useGSAP(
-    () => {
-      gsap.to('.store', { x: -160, scale: 0.5, duration: 3 })
+  // useGSAP(
+  //   () => {
+  //     gsap.to('.store', { x: -160, scale: 0.5, duration: 3 })
 
-      gsap.to('.app', {
-        x: -300,
-        scale: 0.5,
-        duration: 3,
-        onUpdate: function () {
-          gsap.set('.app', { flex: '0 0 100%' })
-        },
-      })
-    },
-    { scope: storeToapp }
-  )
+  //     gsap.to('.app', {
+  //       x: -300,
+  //       scale: 0.5,
+  //       duration: 3,
+  //       onUpdate: function () {
+  //         gsap.set('.app', { flex: '0 0 100%' })
+  //       },
+  //     })
+  //   },
+  //   { scope: storeToapp }
+  // )
+
+  const images = [s1, s2, s3]
+  const [more, openMore] = useState(false)
+  const description = more
+    ? "QuickNews helps you discover balanced, unbiased stories -- for free. Connect with the world around you through editor curation and state-of-the-art algorithms. Whether it's politics, local coverage, sports, or entertainment, felling good about being informed has never been easier."
+    : 'QuickNews helps you discover balanced, unbiased stories -- for free. Connect with the world around you through editor curation and state-of-the-art '
+  const btn = 'Get'
 
   return (
     <div className={styles.main} ref={storeToapp}>
+      {/* Store Page */}
       <div className={`${styles.store} store`}>
-        <img src={up} alt="" className={styles.up} />
-        {/* <Slide />
-        <div className="store-header">Rating&Reviews</div>
-        <span className="content"></span>
-        <div className="store-header">App Privacy</div>
-        <span className="content"></span>
-        <div className="block1"></div>
-        <div className="block2"></div>
-        <div className="block3"></div>
-        <div className="store-header">Information</div>
-        <div className="information"></div> */}
+        <div>
+          <img src={up} className={styles.up} />
+          <button>{btn}</button>
+        </div>
+
+        <Slide images={images} />
+        <div className={styles.down}>
+          <img src={device} style={{ width: '100%' }} />
+          <div className={styles.description}>
+            {description}
+            <span
+              className={more ? styles.nomore : styles.more}
+              onClick={() => openMore(!more)}
+            >
+              more
+            </span>
+          </div>
+        </div>
+        <img src={bottom} className={styles.bottom} />
       </div>
+      {/* App Page */}
       <div className={`${styles.app} app`}>
         <img src={up} alt="" className={styles.up} />
       </div>
