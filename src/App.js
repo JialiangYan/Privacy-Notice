bf86a74c4661583d40c63ba815ff36f1cbab5124
yajialiang <240881275@qq.com>
2024-05-14 00:23:23 -0600

    device detection

--- src/App.js
+++ src/App.js
@@ -6,6 +6,30 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css'
 
 export default function App() {
+  const isMobileDevice = () => {
+    const userAgent = navigator.userAgent.toLowerCase()
+
+    // screen size + touch attr + device detection
+    if (
+      window.innerWidth < 500 &&
+      'ontouchstart' in document.documentElement &&
+      /iphone/.test(userAgent)
+    ) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  if (!isMobileDevice()) {
+    // forbidden
+    return (
+      <div id="forbid">
+        <h1 id="forbid-info">Sorry, please use iPhone to start the study</h1>
+      </div>
+    )
+  }
+
   return (
     <Router>
       <Routes>
