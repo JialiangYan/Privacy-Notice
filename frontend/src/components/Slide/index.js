d776975d40dbddfb0442ac4af2e52333312e63e3
yajialiang <240881275@qq.com>
2024-06-20 11:20:17 -0600

    fix: performance

--- frontend/src/components/Slide/index.js
+++ frontend/src/components/Slide/index.js
@@ -1,18 +1,34 @@
 import React, { memo } from 'react'
 import styles from './index.module.css'
 
-import s1 from '../../assets/store/ss1.webp'
-import s2 from '../../assets/store/ss2.webp'
-import s3 from '../../assets/store/ss3.webp'
-
 function Slide() {
   return (
     <div className={styles.container}>
       <div className={styles.scroll}>
         <div className={styles.row}>
-          <img className={styles.img} src={s1} alt="" />
-          <img className={styles.img} src={s2} alt="" />
-          <img className={styles.img} src={s3} alt="" />
+          <img
+            className={styles.img}
+            src={
+              'https://res.cloudinary.com/dfrapneyb/image/upload/v1718898988/s1.webp'
+            }
+            alt=""
+          />
+          <img
+            className={styles.img}
+            src={
+              'https://res.cloudinary.com/dfrapneyb/image/upload/v1718898989/s2.webp'
+            }
+            alt=""
+          />
+          <img
+            className={styles.img}
+            src={
+              'https://res.cloudinary.com/dfrapneyb/image/upload/v1718898988/s3.webp'
+            }
+            alt=""
+            priority="low"
+            loading="lazy"
+          />
         </div>
       </div>
     </div>
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -56,11 +56,6 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.svg$/,
-        type: 'asset',
-        use: 'svgo-loader',
-      },
-      {
         test: /\.(png|svg|jpg|jpeg|gif|ico|webp)$/,
         exclude: /node_modules/,
         use: [
