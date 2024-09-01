95ada99578e3740d5b742e429174e0f5df1ffe97
yajialiang <240881275@qq.com>
2024-08-31 23:12:18 -0600

    fe: cond

--- backend/controllers/userContorller.js
+++ backend/controllers/userContorller.js
@@ -1,6 +1,7 @@
 const asyncHandler = require('express-async-handler')
 const mongoose = require('mongoose')
 const User = require('../models/userModel')
+const Counter = require('../models/counterModel')
 
 // Helper functions
 
@@ -43,7 +44,18 @@ const createUser = asyncHandler(async (req, res) => {
     // if user has not been created
     try {
       // generate condition
-      const condition = 2
+      let counter = await Counter.findOne()
+      if (!counter) {
+        counter = new Counter({ number: 1 })
+        await counter.save()
+      } else {
+        counter.number++
+        if (counter.number > 9) {
+          counter.number = 1
+        }
+        await counter.save()
+      }
+      const condition = counter.number
       const news = [...returnNews()]
       const user = new User({ id, condition, news }) // create new user
       await user.save()
